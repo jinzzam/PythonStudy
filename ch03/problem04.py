faec9f6336af896797d9baba8cb7a85e7b1ec759
jinzzam <firstmind_@naver.com>
2018-07-02 01:32:57 +0900

    주석 인코딩 문제 해결

--- ch03/problem04.py
+++ ch03/problem04.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 timeterm = input('Enter the time term between lightening and thunder : ')
 distance = timeterm * 340
-print(distance)
-print('m')
-# 숫자 바로 옆에 문자를 어떻게 출력할까?
+print(distance, 'm')
+# 숫자 바로 뒤에 어떻게 문자를 출력할까
+# 주석 인코딩 문제 해결
