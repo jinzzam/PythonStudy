78d001f75602fded41828a1fc906fcc94a4833b0
jinzzam <firstmind_@naver.com>
2018-07-02 01:50:57 +0900

    궁금증 해결

--- ch03/problem04.py
+++ ch03/problem04.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 timeterm = input('Enter the time term between lightening and thunder : ')
 distance = timeterm * 340
-print(distance, 'm')
+d = str(distance)
+print(d + 'm')
 # 숫자 바로 뒤에 어떻게 문자를 출력할까
+# 정답은 바로 str이었다
 # 주석 인코딩 문제 해결
