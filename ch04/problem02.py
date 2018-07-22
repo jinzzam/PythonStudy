83726f98853d10e1dcd2da59421ec6490476fdf2
jinzzam <firstmind_@naver.com>
2018-07-22 15:29:45 +0900

    edit r -> modify target width

--- ch04/problem02.py
+++ ch04/problem02.py
@@ -3,24 +3,25 @@ from graphics import *
 
 def main():
     win = GraphWin()
-    r = 50
-    shape1 = Circle(Point(50, 50), r)
+    r = 10
+    shape1 = Circle(Point(50, 50), 5 * r)
     shape1.setFill("white")
     shape1.draw(win)
-    shape2 = Circle(Point(50, 50), r / 2)
+    shape2 = Circle(Point(50, 50), 4 * r)
     shape2.setFill("black")
     shape2.draw(win)
-    shape3 = Circle(Point(50, 50), r / 4)
+    shape3 = Circle(Point(50, 50), 3 * r)
     shape3.setFill("blue")
     shape3.draw(win)
-    shape4 = Circle(Point(50, 50), r / 8)
+    shape4 = Circle(Point(50, 50), 2 * r)
     shape4.setFill("red")
     shape4.draw(win)
-    shape5 = Circle(Point(50, 50), r / 16)
+    shape5 = Circle(Point(50, 50), r)
     shape5.setFill("yellow")
     shape5.draw(win)
     p = win.getMouse()
     if p:
         win.close()
-main()
 
+
+main()
