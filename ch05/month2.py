91f9ec75ce46eec4d47dbb23e96dd93e2eeadf89
jinzzam <firstmind_@naver.com>
2018-07-24 14:25:44 +0900

    list

---
+++ ch05/month2.py
@@ -0,0 +1,9 @@
+def main():
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November", "December"]
+
+    n = int(raw_input("Enter month : "))
+
+    print("This month is " + months[n - 1])
+
+main()
